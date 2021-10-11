6c098955cc952ce6451723630a56f3fb5d6ab487
Oscar Ore <oscar.ore2011@gmail.com>
2021-10-10 22:33:07 -0400

    finished Clock app

--- src/clock.js
+++ src/clock.js
@@ -1,61 +1,34 @@
-import React from 'react'; 
+import React from 'react'
 
-function Clock() {
-    return(
-        <div className="clock">
-            <div className="hand hour"></div>
-            <div className="hand minute">
-            <div className="hand second">
+function Clock({hourRatio, minuteRatio, secondRatio}) {
 
-                <div className="number 1">
-                    <div>1</div>
-                </div>
-                <div className="number 2">
-                    <div>2 </div>
-                </div>
-                <div className="number 3">
-                    <div>3</div>
-                </div>
-                <div className="number 4">
-                    <div>4</div>
-                </div>
-                <div className="number 5">
-                    <div>5</div>
-                </div>
-                <div className="number 6">
-                    <div>6</div>
-                </div>
-                <div className="number 7">
-                    <div>7</div>
-                </div>
-                <div className="number 8">
-                    <div>8</div>
-                </div>
-                <div className="number 9">
-                    <div>9</div>
-                </div>
-                <div className="number 10">
-                    <div>10</div>
-                </div>
-                <div className="number 11">
-                    <div>11</div>
-                </div>
-                <div className="number 12">
-                    <div>12</div>
-                </div>
+  console.log(secondRatio)
 
 
-
-
-
-
-
-
-
-                </div>  
-            </div>
-        </div>
+    return (
+        
+      <div className="clock">
+        <div className="hand hour" style={{transform: `translate(-50%) rotate(${hourRatio * 360}deg)`}}></div>
+        <div className="hand minute"style={{transform: `translate(-50%) rotate(${minuteRatio * 360}deg)`}}  ></div>
+        <div className="hand second"style={{transform: `translate(-50%) rotate(${secondRatio * 360}deg)`}}  ></div>
+        
+        <div className="number number1"><div>1</div></div>
+        <div className="number number2"><div>2</div></div>
+        <div className="number number3"><div>3</div></div>
+        <div className="number number4"><div>4</div></div>
+        <div className="number number5"><div>5</div></div>
+        <div className="number number6"><div>6</div></div>
+        <div className="number number7"><div>7</div></div>
+        <div className="number number8"><div>8</div></div>
+        <div className="number number9"><div>9</div></div>
+        <div className="number number10"><div>10</div></div>
+        <div className="number number11"><div>11</div></div>
+        <div className="number number12"><div>12</div></div>
+    </div>
+    
     )
+    
 }
 
-export default Clock 
+
+export default Clock
