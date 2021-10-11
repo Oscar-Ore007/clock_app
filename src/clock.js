a9eff1bd64d7b9327f8919bf42bac20c47fc106f
Oscar Ore <oscar.ore2011@gmail.com>
2021-10-10 21:54:10 -0400

    built clock logic

---
+++ src/clock.js
@@ -0,0 +1,61 @@
+import React from 'react'; 
+
+function Clock() {
+    return(
+        <div className="clock">
+            <div className="hand hour"></div>
+            <div className="hand minute">
+            <div className="hand second">
+
+                <div className="number 1">
+                    <div>1</div>
+                </div>
+                <div className="number 2">
+                    <div>2 </div>
+                </div>
+                <div className="number 3">
+                    <div>3</div>
+                </div>
+                <div className="number 4">
+                    <div>4</div>
+                </div>
+                <div className="number 5">
+                    <div>5</div>
+                </div>
+                <div className="number 6">
+                    <div>6</div>
+                </div>
+                <div className="number 7">
+                    <div>7</div>
+                </div>
+                <div className="number 8">
+                    <div>8</div>
+                </div>
+                <div className="number 9">
+                    <div>9</div>
+                </div>
+                <div className="number 10">
+                    <div>10</div>
+                </div>
+                <div className="number 11">
+                    <div>11</div>
+                </div>
+                <div className="number 12">
+                    <div>12</div>
+                </div>
+
+
+
+
+
+
+
+
+
+                </div>  
+            </div>
+        </div>
+    )
+}
+
+export default Clock 
